67bdcb876b54be5af39cde622685316af60b9ce4
Vladimir Benko <vnbenko@gmail.com>
2021-06-14 22:45:02 +0300

    feat: implement logic for exercise 2

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -1,8 +1,16 @@
 import Foundation
 
 class StockMaximize {
-
+    
     func countProfit(prices: [Int]) -> Int {
-        return -1
+        var profit = 0
+        for (index, price) in prices.enumerated() {
+            if let maxPrice = prices.dropFirst(index + 1).max() {
+                if price < maxPrice {
+                    profit += maxPrice - price
+                }
+            }
+        }
+        return profit
     }
 }
